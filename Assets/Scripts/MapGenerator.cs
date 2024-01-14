b15bf521c870f3ed122cffc1c9497a810c9d6a6a
Guilherme Frankenberger <nemseialistar@gmail.com>
2024-01-14 19:09:14 -0300

    add mapgenerator validators

--- Assets/Scripts/MapGenerator.cs
+++ Assets/Scripts/MapGenerator.cs
@@ -6,6 +6,7 @@ public class MapGenerator : MonoBehaviour
     public int mapHeight;
     public float noiseScale;
     public int octaves;
+    [Range(0, 1)]
     public float persistance;
     public float lacunarity;
 
@@ -21,5 +22,25 @@ public class MapGenerator : MonoBehaviour
         MapDisplay mapDisplay = FindObjectOfType<MapDisplay>();
 
         mapDisplay.DrawNoiseMap(noiseMap);
+    }
+
+    void OnValidate()
+    {
+        if (mapWidth < 1)
+        {
+            mapWidth = 1;
+        }
+        if (mapHeight < 1)
+        {
+            mapHeight = 1;
+        }
+        if (lacunarity < 1)
+        {
+            lacunarity = 1;
+        }
+        if (octaves < 0)
+        {
+            octaves = 0;
+        }
     }
 }
